16fceca4013b352ff0f1d3e76f51d433d547ee3e
Nicole Go <nicoleam.go@gmail.com>
2025-02-27 07:14:04 -0500

    Initial Commit

---
+++ TransactionsSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TransactionsSearch
+//
+//  Created by Nicole Go on 2025-02-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TransactionsSearch/TransactionsSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransactionsSearchApp.swift
+//  TransactionsSearch
+//
+//  Created by Nicole Go on 2025-02-27.
+//
+
+import SwiftUI
+
+@main
+struct TransactionsSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
